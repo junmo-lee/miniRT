f2365533b792f47596d957cab802460d0561157c
junmo-lee <junmlee@student.42seoul.kr>
2024-08-28 16:58:34 +0900

    8.3. 장면 설정 예제와 맞추기 위해 999->990

--- src/hit_sphere.c
+++ src/hit_sphere.c
@@ -11,23 +11,11 @@ t_bool      hit_sphere(t_object *sp_obj, t_ray *ray, t_hit_record *rec)
 	double  half_b;
 	double  c;
 	double  discriminant; //판별식
-	/* * * * 추가 * * * */
 	double  sqrtd;
 	double  root;
 
 	sp = sp_obj->element;
 	oc = vminus(ray->orig, sp->center);
-	/* 수정 전
-	a = vdot(ray->dir, ray->dir);
-	b = 2.0 * vdot(oc, ray->dir);
-	c = vdot(oc, oc) - sp->radius2;
-	discriminant = b * b - 4 * a * c;
-	if (discriminant < 0) // 판별식이 0보다 작을 때 : 실근 없을 때,
-		return (-1.0);
-	else
-		return ((-b - sqrt(discriminant)) / (2.0 * a)); // 두 근 중 작은 근
-	수정 전 끝 */
-	/* * * * 수정 * * * */
 	a = vlength2(ray->dir);
 	half_b = vdot(oc, ray->dir);
 	c = vlength2(oc) - sp->radius2;
@@ -48,6 +36,6 @@ t_bool      hit_sphere(t_object *sp_obj, t_ray *ray, t_hit_record *rec)
 	rec->p = ray_at(ray, root);
 	rec->normal = vdivide(vminus(rec->p, sp->center), sp->radius); // 정규화된 법선 벡터.
 	set_face_normal(ray, rec); // rec의 법선벡터와 광선의 방향벡터를 비교해서 앞면인지 뒷면인지 t_bool 값으로 저장.
-	rec->albedo = sp_obj->albedo;
+	// rec->albedo = sp_obj->albedo;
 	return (TRUE);
 }
--- src/trace.h
+++ src/trace.h
@@ -15,4 +15,7 @@ t_bool      hit(t_object *obj, t_ray *ray, t_hit_record *rec);
 t_bool      hit_obj(t_object *obj, t_ray *ray, t_hit_record *rec);
 t_bool      hit_sphere(t_object *world, t_ray *ray, t_hit_record *rec);
 void        set_face_normal(t_ray *r, t_hit_record *rec);
+
+t_color3        phong_lighting(t_scene *scene);
+
 #endif
